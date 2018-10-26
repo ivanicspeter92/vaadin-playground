558202696d5b8789f7efe34651b4419ce80b64d3
Peter Ivanics <peter@choicely.com>
2018-10-26 13:17:59 +0200

    Added filtering

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,7 +1,13 @@
 package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
@@ -11,21 +17,42 @@ import com.vaadin.flow.server.PWA;
 @Route("")
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
+    private TextField filterText = new TextField();
+    private Button clearFilterTextBtn = new Button(new Icon(VaadinIcon.CLOSE_CIRCLE));
+    private Grid<Customer> grid = new Grid<>();
 
     public MainView() {
-        CustomerService service = CustomerService.getInstance();
-        Grid<Customer> grid = new Grid<>();
+        configureFilterTextField();
+        configureClearFilterButton();
+        HorizontalLayout filtering = new HorizontalLayout(filterText, clearFilterTextBtn);
 
+        configureCustomerGrid();
+        updateList();
+        add(filtering, grid);
+
+        setHeight("100vh");
+    }
+
+    private void configureFilterTextField() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setValueChangeMode(ValueChangeMode.EAGER);
+        filterText.addValueChangeListener(e -> updateList());
+    }
+
+    private void configureClearFilterButton() {
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+    }
+
+    private void configureCustomerGrid() {
         grid.setSizeFull();
 
         grid.addColumn(Customer::getId).setHeader("ID");
         grid.addColumn(Customer::getFirstName).setHeader("First name");
         grid.addColumn(Customer::getLastName).setHeader("Last name");
         grid.addColumn(Customer::getStatus).setHeader("Status");
+    }
 
-        add(grid);
-        setHeight("100vh");
-
-        grid.setItems(service.findAll());
+    private void updateList() {
+        grid.setItems(CustomerService.getInstance().findAll(filterText.getValue()));
     }
 }
